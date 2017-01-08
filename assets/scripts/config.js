c27e7a2d5d64156cf702af8c7883db13f8eb68bb
rickdean <rickdean@outlook.com>
2017-01-08 12:51:55 -0500

    First Commit to GH Pages
    GH Pages Commit

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const config = {
-  host: 'http://localhost:4741'
+  // host: 'http://localhost:4741'
+  host: 'https://gentle-hamlet-35507.herokuapp.com'
 };
 
 module.exports = config;
